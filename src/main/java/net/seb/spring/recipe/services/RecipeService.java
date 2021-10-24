da8142f2749cf99a4538235883e40de0df3b02ee
Sebastian Ostafiev <sebastian.ostafiev@yahoo.com>
2021-10-25 00:43:51 +0300

    Update a recipe.

--- src/main/java/net/seb/spring/recipe/services/RecipeService.java
+++ src/main/java/net/seb/spring/recipe/services/RecipeService.java
@@ -11,4 +11,5 @@ public interface RecipeService {
     Set<Recipe> getRecipes();
     Recipe findById(Long l);
     RecipeCommand saveRecipeCommand(RecipeCommand command);
+    RecipeCommand findCommandById(Long l);
 }
