fa96ae789508efbea254c3feb18a1dfc552d41da
Sebastian Ostafiev <sebastian.ostafiev@yahoo.com>
2021-10-25 01:35:07 +0300

    Delete a recipe.

--- src/main/java/net/seb/spring/recipe/controllers/RecipeController.java
+++ src/main/java/net/seb/spring/recipe/controllers/RecipeController.java
@@ -1,5 +1,6 @@
 package net.seb.spring.recipe.controllers;
 
+import lombok.extern.slf4j.Slf4j;
 import net.seb.spring.recipe.commands.RecipeCommand;
 import net.seb.spring.recipe.services.RecipeService;
 import org.springframework.stereotype.Controller;
@@ -7,7 +8,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.PostConstruct;
-
+@Slf4j
 @Controller
 public class RecipeController {
     private final RecipeService recipeService;
@@ -16,18 +17,21 @@ public class RecipeController {
         this.recipeService = recipeService;
     }
 
+    @GetMapping
     @RequestMapping("/recipe/{id}/show")
     public String showById(@PathVariable String id, Model model) {
         model.addAttribute("recipe", recipeService.findById(new Long(id)));
         return "recipe/show";
     }
 
+    @GetMapping
     @RequestMapping("recipe/new")
     public String newRecipe(Model model) {
         model.addAttribute("recipe", new RecipeCommand());
         return "recipe/recipeform";
     }
 
+    @GetMapping
     @RequestMapping("recipe/{id}/update")
     public String updateRecipe(@PathVariable String id, Model model) {
         model.addAttribute("recipe", recipeService.findCommandById(Long.valueOf(id)));
@@ -40,5 +44,13 @@ public class RecipeController {
     public String saveOrUpdate(@ModelAttribute RecipeCommand command) {
         RecipeCommand savedCommand = recipeService.saveRecipeCommand(command);
         return "redirect:/recipe/" + savedCommand.getId() + "/show";
+    }
+
+    @GetMapping
+    @RequestMapping("recipe/{id}/delete")
+    public String deleteById(@PathVariable String id) {
+        log.debug("Deleting id: " + id);
+        recipeService.deleteById(Long.valueOf(id));
+        return "redirect:/";
     }
 }
