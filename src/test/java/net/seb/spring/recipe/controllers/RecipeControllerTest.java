f8d0a3d8d2fb439137b4834ec7268f75165766f4
Sebastian Ostafiev <sebastian.ostafiev@yahoo.com>
2021-11-07 23:46:13 +0200

    Handle Number Format Exception.

--- src/test/java/net/seb/spring/recipe/controllers/RecipeControllerTest.java
+++ src/test/java/net/seb/spring/recipe/controllers/RecipeControllerTest.java
@@ -65,6 +65,16 @@ public class RecipeControllerTest {
     }
 
     @Test
+    public void testGetRecipeNumberFormatException() throws Exception {
+
+        //when(recipeService.findById(anyLong())).thenThrow(NotFoundException.class);
+
+        mockMvc.perform(get("/recipe/dsfdf/show"))
+                .andExpect(status().isBadRequest())
+                .andExpect(view().name("400error"));
+    }
+
+    @Test
     public void testGetNewRecipeForm() throws Exception {
         RecipeCommand command = new RecipeCommand();
 
