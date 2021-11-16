1520e9b5b44666282d1c18c0de6f1313d835561b
Sebastian Ostafiev <sebastian.ostafiev@yahoo.com>
2021-11-17 00:57:18 +0200

    Schema generation with hibernate.

--- src/main/scripts/configure-mysql.sql
+++ src/main/scripts/configure-mysql.sql
@@ -1,18 +1,18 @@
-## Use to run mysql db docker image, optional if youre not using a local mysqldb
-# docker run --name mysqldb -p 3306:3306 -e MYSQL_ALLOW_EMPTY_PASSWORD=yes -d mysql
+-- Use to run mysql db docker image, optional if you're not using a local mysqldb
+-- docker run --name mysqldb -p 3306:3306 -e MYSQL_ALLOW_EMPTY_PASSWORD=yes -d mysql
 
-# connect to mysql and run as root user
-# Create Databases
+-- connect to mysql and run as root user
+-- Create Databases
 CREATE DATABASE sfg_dev;
 CREATE DATABASE sfg_prod;
 
-# Create database service accounts
+-- Create database service accounts
 CREATE USER 'sfg_dev_user'@'localhost' IDENTIFIED BY 'guru';
 CREATE USER 'sfg_prod_user'@'localhost' IDENTIFIED BY 'guru';
 CREATE USER 'sfg_dev_user'@'%' IDENTIFIED BY 'guru';
 CREATE USER 'sfg_prod_user'@'%' IDENTIFIED BY 'guru';
 
-# Database grants
+-- Database grants
 GRANT SELECT ON sfg_dev.* to 'sfg_dev_user'@'localhost';
 GRANT INSERT ON sfg_dev.* to 'sfg_dev_user'@'localhost';
 GRANT DELETE ON sfg_dev.* to 'sfg_dev_user'@'localhost';
